51fd7b3f6f1eda617f912deb57d082451b85b251
Laurent Payot <laurent.payot@publiberty.com>
2023-10-28 00:57:34 +0200

    JS custom event fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { start } from '../output/Main/index.js'
 
 setInterval(() => {
-    new CustomEvent("tick", { time: new Date().toISOString() })
+    new CustomEvent("tick",{ detail: JSON.stringify(new Date().toISOString()) })
 }, 1000)
 
 start({ initialCount: 100})()
