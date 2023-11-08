33c367f981375cf20be979bd95b00eea5ef96fcd
Laurent Payot <laurent.payot@publiberty.com>
2023-11-08 18:14:50 +0100

    catBase64JS

--- src/ps/Main.js
+++ src/ps/Main.js
@@ -4,6 +4,5 @@ export const catBase64JS = async (text) => {
     console.log("*** text = ", text)
     const response = await fetch(`https://cataas.com/cat/says/${text}?fontSize=50&fontColor=red`)
     const array = await response.body.getReader().read()
-    const base64 = btoa(String.fromCharCode.apply(null, array.value))
-    return base64
+    return btoa(String.fromCharCode.apply(null, array.value))
 }
