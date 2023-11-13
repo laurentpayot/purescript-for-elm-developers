22c320d50e78a5e02c221175a2bdebdc59a6b9de
Laurent <laurent.payot@publiberty.com>
2023-11-13 23:03:04 +0100

    catBase64JS null fixes

--- src/ps/Main.js
+++ src/ps/Main.js
@@ -5,6 +5,6 @@ export const catBase64JS = text => fontSize =>
     //  Wrapping in a PS Effect with the "thunk" `() =>` is needed so function is not considered pure and is run every time
     async () => {
         const response = await fetch(`https://cataas.com/cat/says/${text}?fontSize=${fontSize}&fontColor=red`)
-        const array = await response.body.getReader().read()
-        return btoa(String.fromCharCode.apply(null, array.value))
+        const array = await response.body?.getReader().read()
+        return btoa(String.fromCharCode.apply(null, array?.value))
     }
